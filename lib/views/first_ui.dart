cabbc7706c3677777564890441a5eef0cbe2c61a
s6252410004 <s6252410004 @sau.ac.th>
2021-08-27 10:24:55 +0700

    Cobe V.1 write name

---
+++ lib/views/first_ui.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class FirstUI extends StatelessWidget {
+  const FirstUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.blueAccent,
+      appBar: AppBar(
+        title: Text('Second Impact'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.black87,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body: Center( 
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center ,
+          children: [
+            Text(
+              'Chanatip Keawpreasert',
+              style: TextStyle(
+                fontSize: 30.0, 
+                fontWeight: FontWeight.bold,
+                color: Colors.redAccent[400],
+              ),
+            ),
+            Text(
+              '6252410004',
+              style: TextStyle(
+                fontSize: 25.0, 
+                fontWeight: FontWeight.normal,
+                color: Colors.redAccent[700],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
